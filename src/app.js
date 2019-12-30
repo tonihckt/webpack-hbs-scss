440fc751e04a8b2533a0aefc4f89baca7b29e774
Toni Hackett <hckt76@gmail.com>
2019-12-30 12:50:56 +0100

    first commit

---
+++ src/app.js
@@ -0,0 +1,23 @@
+// Add Scripts ---------------
+// ---------------------------
+import './scripts/main.js';
+
+// Add Styles -----------------
+// ----------------------------
+import './styles/main.scss';
+
+
+// Add Libs -----------------
+// --------------------------
+
+//Bootstrap
+import 'bootstrap';
+// Alternatively, you may import plugins individually as needed:
+// import 'bootstrap/js/dist/util';
+// import 'bootstrap/js/dist/dropdown';
+
+//Fontawesome
+import '@fortawesome/fontawesome-free/js/fontawesome'
+import '@fortawesome/fontawesome-free/js/solid'
+import '@fortawesome/fontawesome-free/js/regular'
+import '@fortawesome/fontawesome-free/js/brands'
